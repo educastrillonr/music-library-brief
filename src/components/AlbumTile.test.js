4b411266cbd73230abdcfa80d47f0fdc45bc92f5
Eduardo Romero <eduardo.romero@Nology.local>
2019-08-02 17:07:48 +0100

    updater for tests

---
+++ src/components/AlbumTile.test.js
@@ -0,0 +1,46 @@
+import AlbumTile from "./AlbumTile";
+import React from "react";
+import clash from "../data/images/clash.jpg";
+import { mount } from "enzyme";
+
+const mockData = {
+  artist: "The Clash",
+  albumName: "London Calling",
+  genre: "punk",
+  released: 1979,
+  image: clash
+};
+
+describe("AlbumTile Tests", () => {
+  let component;
+  // beforeEach() provides setup to run prior to every test
+
+  beforeEach(() => {
+    component = mount(<AlbumTile album={mockData} />);
+  });
+
+  it("should contain a HTML image tag", () => {
+    expect(component.find("img").length).toEqual(1);
+  });
+
+  it("should receive data passed as props from parent", () => {
+    expect(component.props().album).toEqual(mockData);
+  });
+  //   it("should render data passed in as props", () => {
+  //     expect(component.find(mockData.image)).toBeTruthy();
+  //   });
+
+  it("should toggle isClicked state through handleClick method", () => {
+    expect(component.state("isClicked")).toEqual(false);
+    component.instance().handleClick();
+    expect(component.state("isClicked")).toEqual(true);
+    component.instance().handleClick();
+    expect(component.state("isClicked")).toEqual(false);
+  });
+
+  it("should update isClicked state when component is clicked", () => {
+    expect(component.state("isClicked")).toEqual(false);
+    component.find("article").simulate("click");
+    expect(component.state("isClicked")).toEqual(true);
+  });
+});
